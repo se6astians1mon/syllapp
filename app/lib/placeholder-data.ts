c23843c10a19aba900b935f7e6fbf22ef53af5fe
unknown <bachti060293@gmail.com>
2025-12-11 21:47:54 +0100

    added buttons

---
+++ app/lib/placeholder-data.ts
@@ -0,0 +1,18 @@
+const kids = [
+  {
+    kid_id: "one",
+    syllabus_lists: [
+      {"name": "syllabus1", "list": ["mo", "ro", "to"]},
+      {"name": "syllabus2", "list": ["di", "mi", "fr"]},
+      {"name": "syllabus3", "list": ["sa", "so"]},
+    ],
+  },
+  {
+    kid_id: "two",
+    syllabus_lists: [
+      {"name": "syllabusA", "list": ["mo", "di", "mi"]}, 
+      {"name": "syllabusB", "list": ["do", "fr"]},
+    ],
+  },
+  // ...
+];
